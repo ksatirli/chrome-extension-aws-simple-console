78a34384341adaac9136a2d9f7072d3bf86c125c
Kerim Satirli <kerim@confkit.com>
2015-10-13 21:40:50 +0200

    adds imagery

---
+++ src/images/generate-icons.sh
@@ -0,0 +1,13 @@
+convert -density "1200" -resize 16x16 "icon.svg" icon-16.png
+
+convert -density "1200" -resize 32x32 "icon.svg" icon-32.png
+
+convert -density "1200" -resize 48x48 "icon.svg" icon-48.png
+
+convert -density "1200" -resize 64x64 "icon.svg" icon-64.png
+
+convert -density "1200" -resize 96x96 "icon.svg" icon-96.png
+
+convert -density "1200" -resize 128x128 "icon.svg" icon-128.png
+
+convert -density "1200" -resize 256x256 "icon.svg" icon-256.png
